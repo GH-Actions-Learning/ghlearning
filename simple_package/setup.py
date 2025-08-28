9d66e8b56b5965f6d6e866d5b7d97a6547e96ce1
SAI SRUJAN <112695986+SaiSrujan-Asara@users.noreply.github.com>
2025-08-28 21:47:05 +0530

    Update setup.py

--- simple_package/setup.py
+++ simple_package/setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="sample_pkg",
-    version="0.0.1",
-    packages=["sample_pkg"],
-    description="A simple test package",
+    name="sample_pkg",                                       # The name used when installing: pip install sample_pkg
+    version="0.0.1",                                         # Initial version
+    author="Rich",
+    author_email="rich@rich.com",
+    description="A very basic Python package example",
+    packages=find_packages(),                                # Automatically finds 'sample_pkg'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",                                # Minimum Python version
 )
